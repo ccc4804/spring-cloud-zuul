29b6cf1db1e68c654c0c067eefa3d97b1ee1ede6
coco <hwchae@mommaeat.com>
2021-02-02 16:02:49 +0900

    discovery 설정

---
+++ test-service/src/main/java/com/test/Controller.java
@@ -0,0 +1,20 @@
+package com.test;
+
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class Controller {
+  @GetMapping
+  public ResponseEntity<String> test() {
+    return ResponseEntity.ok().body("success");
+  }
+
+  @GetMapping("/ping")
+  public ResponseEntity<String> ping() {
+    return ResponseEntity.ok().body("pong");
+  }
+}
