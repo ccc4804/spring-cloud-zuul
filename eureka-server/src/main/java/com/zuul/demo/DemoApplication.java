86d7cad832b3425612722ef8d3d204edf146efd5
coco <hwchae@mommaeat.com>
2021-02-03 12:30:16 +0900

    eureka server에 zuul 같이 사용

--- eureka-server/src/main/java/com/zuul/demo/DemoApplication.java
+++ eureka-server/src/main/java/com/zuul/demo/DemoApplication.java
@@ -2,9 +2,13 @@ package com.zuul.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.netflix.eureka.EnableEurekaClient;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
+import org.springframework.cloud.netflix.zuul.EnableZuulProxy;
 
 @EnableEurekaServer
+@EnableEurekaClient
+@EnableZuulProxy
 @SpringBootApplication
 public class DemoApplication {
 
